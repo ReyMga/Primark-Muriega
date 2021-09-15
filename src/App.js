ee7e8cd9bda9f52da0f0dc84a2bf204112960904
Raquel Muriega <muriega.raquel@gmail.com>
2021-09-15 15:32:25 -0300

    menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import theme from './temaConfig'
+import {ThemeProvider} from '@material-ui/core/styles';
 import './App.css';
+import Navbar from './components/NavBar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Navbar/>
+   
+      
+    </ThemeProvider>
   );
 }
 
