8360b7e4c6ecb22762be407d03e12e8c6d6c73bb
Raquel Muriega <muriega.raquel@gmail.com>
2021-10-26 17:54:14 -0300

    firebase II

--- src/components/Modals/SuccessFormModal.js
+++ src/components/Modals/SuccessFormModal.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { Button, Modal } from "react-bootstrap";
+import "./styles.css";
 
 const SuccessFormModal = ({ show, handleClose, id }) => {
   return (
     <Modal show={show} onHide={handleClose} animation={false}>
-      <Modal.Header closeButton>
-        <Modal.Title>Datos del Comprador</Modal.Title>
-      </Modal.Header>
+      <Modal.Title>Datos del Comprador</Modal.Title>
       Orden generada correctamente con el siguiente ID: {id}
       <Modal.Footer>
-        <Button variant="primary" onClick={handleClose}>
+        <Button className="form" variant="primary" onClick={handleClose}>
           Aceptar
         </Button>
       </Modal.Footer>
